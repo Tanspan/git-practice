89222c48ef93a88d9187b88a401600bc45217bd5
tanspan <tanusrikaranam@gmail.com>
2026-01-03 14:45:44 +0530

    Added show tasks

--- todo.py
+++ todo.py
@@ -2,6 +2,12 @@ tasks = [1,2,3,4,5]
 
 def show_tasks():
     print(tasks)
+def show_tasks():
+    if not tasks:
+        print("No tasks available")
+    else:
+        for i, task in enumerate(tasks):
+            print(f"{i+1}. {task}")
 
 if __name__ == "__main__":
     show_tasks()
