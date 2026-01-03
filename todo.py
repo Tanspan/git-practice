9328aa3c309c73648771865fa8167495d183f68e
tanspan <tanusrikaranam@gmail.com>
2026-01-03 14:46:54 +0530

    Added delete task

--- todo.py
+++ todo.py
@@ -2,6 +2,12 @@ tasks = [1,2,3,4,5]
 
 def show_tasks():
     print(tasks)
+def delete_task(index):
+    if index < len(tasks):
+        tasks.pop(index)
+        print("Task deleted")
+    else:
+        print("Invalid index")
 
 if __name__ == "__main__":
     show_tasks()
