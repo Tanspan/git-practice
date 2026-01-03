b54af8af2564d08aeb5b2e36f79e1b21dd8eec79
tanspan <tanusrikaranam@gmail.com>
2026-01-03 16:03:22 +0530

    Now atleast

--- todo.py
+++ todo.py
@@ -1,21 +1,7 @@
 tasks = [1,2,3,4,5]
 
-def show_tasks():
-    if not tasks:
-        print("No tasks available")
-    else:
-        for i, task in enumerate(tasks):
-            print(f"{i+1}. {task}")
-
 def add_tasks(task):
     tasks.append(task)
 
-def delete_task(index):
-    if index < len(tasks):
-        tasks.pop(index)
-        print("Task deleted")
-    else:
-        print("Invalid index")
-
 if __name__ == "__main__":
-    show_tasks()
+    add_tasks()
